fdbff1798214e04a7c4fcc067e7346d9a61f9288
Artem <45176000+ArtemAsadov@users.noreply.github.com>
2020-03-04 23:20:54 +0300

    #ADV-15: [Common] Интегрировать Web GUI в солюшен (#10)
    * Доработал  открытие внешней стартовой страницы
    
    * Исправления

--- src/engine/AdventureEngine/AdventureEngine/Startup.cs
+++ src/engine/AdventureEngine/AdventureEngine/Startup.cs
@@ -1,18 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using AdventureEngine.Models;
 using AdventureEngine.Models.Interface;
-using Microsoft.AspNetCore.Http;
 
 namespace AdventureEngine
 {
@@ -49,19 +41,15 @@ namespace AdventureEngine
             //        + System.Diagnostics.Process.GetCurrentProcess().ProcessName);
             //});
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
+
+            app.UseDefaultFiles();
             app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                     name: "default",
-                     pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
         }
     }
 }
