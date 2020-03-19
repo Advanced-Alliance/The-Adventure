0c3c91e03181568168c44482f43af769fdb4c95b
Artem <45176000+ArtemAsadov@users.noreply.github.com>
2020-03-19 22:50:34 +0300

    [Bug] Откатил удаление контроллера (#12)

--- src/engine/AdventureEngine/AdventureEngine/Startup.cs
+++ src/engine/AdventureEngine/AdventureEngine/Startup.cs
@@ -50,6 +50,12 @@ namespace AdventureEngine
 
             app.UseAuthorization();
 
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                     name: "default",
+                     pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
